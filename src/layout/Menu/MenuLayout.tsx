c1b675932c57ce5c1f7a11a6500c780476b4295c
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-01-08 23:26:25 +0200

    Part 2: React Router > Exercise > Creating a Layout

--- src/layout/Menu/MenuLayout.tsx
+++ src/layout/Menu/MenuLayout.tsx
@@ -1,11 +1,33 @@
 import {Link, Outlet} from 'react-router-dom';
+import styles from './Layout.module.css';
+import Button from '../../components/Button/Button.tsx';
 
 export function Layout() {
 	return <>
-		<div>
-			<div>
-				<Link to='/'>Menu</Link>
-				<Link to='/cart'>Cart</Link>
+		<div className={styles['layout']}>
+			<div className={styles['sidebar']}>
+				<div className={styles['user']}>
+					<img className={styles['avatar']} src='/avatar.png' alt='Avatar'/>
+					<div className={styles['name']}>
+                        Current User
+					</div>
+					<div className={styles['email']}>
+                        current@mail.com
+					</div>
+
+				</div>
+				<div className={styles['menu']}>
+					<Link to='/' className={styles['link']}>
+						<img src='/menu-icon.svg' alt='Menu icon'/>
+                        Menu</Link>
+					<Link to='/cart' className={styles['link']}>
+						<img src='/cart-icon.svg' alt='Cart icon'/>
+                        Cart</Link>
+				</div>
+				<Button className={styles['exit']}>
+					<img src='/exit-icon.svg' alt='Exit icon'/>
+                    Logout
+				</Button>
 			</div>
 			<div>
 				<Outlet/>
