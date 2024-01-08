a911b1e3af5bf1573150118078d5c9e8befc1d27
ToyLess78 <121128928+ToyLess78@users.noreply.github.com>
2024-01-08 18:01:43 +0200

    Part 1: Transition to TypeScript > Preparing project styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-
-import './App.css'
 import Button from './components/Button/Button.tsx';
 import {useState, MouseEvent} from 'react';
 
 function App() {
 
-    const [counter, setCounter] = useState<number>(0);
-    const addCounter = (e: MouseEvent) => {
-        console.log(e);
-    }
+	const [counter, setCounter] = useState<number>(0);
+	const addCounter = (e: MouseEvent) => {
+		console.log(e);
+	};
 
-  return (
-    <>
-<Button onClick={addCounter}>Button</Button>
-    </>
-  )
+	return (
+		<>
+			<Button onClick={addCounter}>Button</Button>
+		</>
+	);
 }
 
-export default App
+export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,4 @@
-import './Button.module.css';
+import styles from './Button.module.css';
 import {ButtonProps} from './Button.props.ts';
 import cn from 'classnames';
 import {FC} from 'react';
@@ -6,15 +6,16 @@ import {FC} from 'react';
 // Alternative create component type
 export const ButtonAlt: FC<ButtonProps> = ({children, className, ...props}) => {
 
-    return (
-        <button className={cn('button accent', className)} {...props}>{children}</button>
-    );
-}
+	return (
+		<button className={cn(styles['button'], styles['accent'], className)} {...props}>{children}</button>
+	);
+};
+
 function Button({children, className, ...props}: ButtonProps) {
 
-    return (
-        <button className={cn('button accent', className)} {...props}>{children}</button>
-    );
+	return (
+		<button className={cn(styles['button'], styles['accent'], className)} {...props}>{children}</button>
+	);
 }
 
 export default Button;
